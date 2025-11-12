50176ccf0087c9250b274a76e2e09d7ca9f5ed74
lrbn86 <brandonnguyendev@gmail.com>
2025-11-12 17:19:44 -0500

    add entry point

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('combined'));
+
+export default app;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+import app from './app.js';
 
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
