0b40e3c3d5979176f60a69338e3d3c88cb01b50f
lrbn86 <brandonnguyendev@gmail.com>
2025-11-12 17:54:04 -0500

    add auth router

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import authRouter from './src/auth/auth.router.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('combined'));
+
+app.use('/api/auth', authRouter);
 
 export default app;
