f0ffcefb4e8fb14d103439bb5bd689533c353f14
lrbn86 <brandonnguyendev@gmail.com>
2025-11-14 01:14:20 -0500

    add rate limit middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { rateLimit } from 'express-rate-limit';
 import authRouter from './src/auth/auth.router.js';
 import movieRouter from './src/movie/movie.router.js';
 import reservationRouter from './src/reservation/reservation.router.js';
@@ -12,6 +13,7 @@ app.use(helmet());
 
 if (process.env.NODE_ENV !== 'test') {
   app.use(morgan('combined'));
+  app.use(rateLimit());
 }
 
 app.use('/api/auth', authRouter);
