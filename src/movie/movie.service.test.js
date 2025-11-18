e12a79291dd2127fcdc5217980c9f9602675f768
Brandon Nguyen <brandonnguyendev@gmail.com>
2025-11-17 21:03:17 -0500

    add service tests (#2)
    Add tests for movieService.createMovie

--- src/movie/movie.service.test.js
+++ src/movie/movie.service.test.js
@@ -4,11 +4,30 @@ import movieRepository from './movie.repository.js';
 import movieService from './movie.service.js';
 
 describe('Movie Service Test', () => {
-  describe('movieService.createMovie', async (t) => {
+  describe('movieService.createMovie', async () => {
     it('should throw an error if both title and description are not provided', async () => {
       await assert.rejects(async () => await movieService.createMovie(), Error('Title and description are required'));
     });
 
+    it('should throw an error if both title and description are not provided (only title provided)', async () => {
+      await assert.rejects(async () => await movieService.createMovie({ title: 'Title' }), Error('Title and description are required'));
+    });
+
+    it('should throw an error if both title and description are not provided (only description provided)', async () => {
+      await assert.rejects(async () => await movieService.createMovie({ description: 'Description' }), Error('Title and description are required'));
+    });
+
+    it('should call movieRepository.create', async (t) => {
+      t.mock.method(movieRepository, 'create', async () => { });
+      await movieService.createMovie({ title: 'Title', description: 'Description' });
+      assert.equal(movieRepository.create.mock.callCount(), 1);
+    });
+
+    it('should return a movie object', async (t) => {
+      t.mock.method(movieRepository, 'create', async (user) => user);
+      const movie = await movieService.createMovie({ title: 'Title', description: 'Description' });
+      assert.deepEqual(movie, { title: 'Title', description: 'Description' });
+    });
   });
 
 
