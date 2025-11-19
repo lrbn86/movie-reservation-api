50f3dd9d8b3044367fdb3a2db28dd9907485561b
lrbn86 <brandonnguyendev@gmail.com>
2025-11-19 00:32:44 -0500

    check for jwt secret key

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -8,9 +8,12 @@ async function authenticate(req, res, next) {
   }
 
   try {
+    if (!process.env.JWT_SECRET) {
+      throw new Error('JWT secret key required for verifying');
+    }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
-    console.log(req.user);
     next();
   } catch (err) {
     return res.status(401).json({ error: 'Invalid or expired token' });
