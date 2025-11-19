97b99886628a151625e9d11a855df620b461a1d9
lrbn86 <brandonnguyendev@gmail.com>
2025-11-19 00:01:42 -0500

    reimplement auth features

--- src/auth/auth.repository.js
+++ src/auth/auth.repository.js
@@ -1,14 +1,7 @@
 import db from '../util/db.js';
 
-async function create(user) {
-  const result = await db.insert('users', { email: user.email, password: user.password, role: 'user' });
-  return result;
-}
-
-async function findByEmail(user) {
-  const result = await db.findByEmail('users', { email: user.email });
-  return result;
-}
+async function create(user) { }
+async function findByEmail(user) { }
 
 export default {
   create,
