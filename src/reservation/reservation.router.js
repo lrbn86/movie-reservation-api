b558a840ca056ee37a8ad645cff717dc809e84da
lrbn86 <brandonnguyendev@gmail.com>
2025-11-20 14:44:02 -0500

    change at router too

--- src/reservation/reservation.router.js
+++ src/reservation/reservation.router.js
@@ -3,7 +3,7 @@ import reservationController from './reservation.controller.js';
 
 const router = express.Router();
 
-router.post('/', reservationController.addReservation);
+router.post('/', reservationController.createReservation);
 router.get('/', reservationController.getReservations);
 router.get('/:reservationId', reservationController.getReservation);
 router.put('/:reservationId', reservationController.updateReservation);
