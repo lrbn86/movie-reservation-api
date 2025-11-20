55213df6bce147e2cc721cf38709dbfe65fee689
lrbn86 <brandonnguyendev@gmail.com>
2025-11-19 23:55:25 -0500

    implemented adding reservation functionality
    * Implemented controller's addReservation and tests
    * Created stubs for reservation repository
    * Implemented service's createReservation and cleaned up tests for now

---
+++ src/reservation/reservation.controller.test.js
@@ -0,0 +1,53 @@
+import request from 'supertest';
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import reservationService from './reservation.service.js';
+import app from '../../app.js';
+
+describe('Reservation Controller Test', () => {
+  describe('reservation.addReservation', () => {
+    it('should create a reservation with valid inputs and return 201', async (t) => {
+      const mockData = { title: 'Title', seats: 2, date: new Date('11-19-2025').toISOString() };
+      t.mock.method(reservationService, 'createReservation', async () => mockData);
+      const res = await request(app)
+        .post('/api/reservations')
+        .send(mockData);
+
+      assert.equal(res.statusCode, 201);
+      assert.deepEqual(res.body, mockData);
+    });
+
+    it('should return 400 if title is missing', async (t) => {
+      t.mock.method(reservationService, 'createReservation', async () => { });
+      const mockData = { seats: 2, date: new Date('11-19-2025').toISOString() };
+      const res = await request(app)
+        .post('/api/reservations')
+        .send(mockData);
+
+      assert.equal(res.statusCode, 400);
+      assert.deepEqual(res.body, { error: 'Missing title' });
+    });
+
+    it('should return 400 if seats is missing', async (t) => {
+      t.mock.method(reservationService, 'createReservation', async () => { });
+      const mockData = { title: 'Title', date: new Date('11-19-2025').toISOString() };
+      const res = await request(app)
+        .post('/api/reservations')
+        .send(mockData);
+
+      assert.equal(res.statusCode, 400);
+      assert.deepEqual(res.body, { error: 'Missing seats' });
+    });
+
+    it('should return 400 if date is missing', async (t) => {
+      t.mock.method(reservationService, 'createReservation', async () => { });
+      const mockData = { title: 'Title', seats: 2 };
+      const res = await request(app)
+        .post('/api/reservations')
+        .send(mockData);
+
+      assert.equal(res.statusCode, 400);
+      assert.deepEqual(res.body, { error: 'Missing date' });
+    });
+  });
+});
--- src/reservation/reservation.repository.js
+++ src/reservation/reservation.repository.js
@@ -1,1 +1,16 @@
-function insert() { }
+async function create(data) {
+  return data;
+}
+
+async function getAll() { }
+async function getById() { }
+async function update() { }
+async function remove() { }
+
+export default {
+  create,
+  getAll,
+  getById,
+  update,
+  remove,
+};
