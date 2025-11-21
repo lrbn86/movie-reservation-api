ce6d96eaf7647a8bdd932c303524c791c5f4ad34
Brandon Nguyen <brandonnguyendev@gmail.com>
2025-11-21 13:15:22 -0500

    fix reservation controller (#15)
    * changed required fields
    * compact error for missing fields
    * redo tests to reflect above

--- src/reservation/reservation.controller.test.js
+++ src/reservation/reservation.controller.test.js
@@ -1,53 +1,70 @@
 import request from 'supertest';
-import { describe, it } from 'node:test';
+import { describe, it, before, mock } from 'node:test';
 import assert from 'node:assert';
-import reservationService from './reservation.service.js';
+import reservationRepository from './reservation.repository.js';
 import app from '../../app.js';
 
 describe('Reservation Controller Test', () => {
-  describe('reservationController.createReservation', () => {
-    it('should create a reservation with valid inputs and return 201', async (t) => {
-      const mockData = { title: 'Title', seats: 2, date: new Date('11-19-2025').toISOString() };
-      t.mock.method(reservationService, 'createReservation', async () => mockData);
-      const res = await request(app)
-        .post('/api/reservations')
-        .send(mockData);
+  before(() => {
+    mock.method(reservationRepository, 'findShowtime', async () => true);
+    mock.method(reservationRepository, 'findSeats', async () => true);
+  });
 
-      assert.equal(res.statusCode, 201);
-      assert.deepEqual(res.body, mockData);
-    });
+  it('should create a reservation with valid inputs and return 201', async (t) => {
+    const reservationRequest = {
+      userId: 'userId',
+      movieId: 'movieId',
+      showtimeId: 'showtimeId',
+      seats: ['A16'],
+    };
+    const expected = { status: 'reserved', ...reservationRequest };
+    const res = await request(app)
+      .post('/api/reservations')
+      .send(reservationRequest);
 
-    it('should return 400 if title is missing', async (t) => {
-      t.mock.method(reservationService, 'createReservation', async () => { });
-      const mockData = { seats: 2, date: new Date('11-19-2025').toISOString() };
-      const res = await request(app)
-        .post('/api/reservations')
-        .send(mockData);
+    assert.equal(res.statusCode, 201);
+    assert.deepEqual(res.body, expected);
+  });
 
-      assert.equal(res.statusCode, 400);
-      assert.deepEqual(res.body, { error: 'Missing title' });
-    });
+  it('should return 400 if missing movieId', async () => {
+    const reservationRequest = {
+      userId: 'userId',
+      showtimeId: 'showtimeId',
+      seats: ['A16'],
+    };
+    const res = await request(app)
+      .post('/api/reservations')
+      .send(reservationRequest);
 
-    it('should return 400 if seats is missing', async (t) => {
-      t.mock.method(reservationService, 'createReservation', async () => { });
-      const mockData = { title: 'Title', date: new Date('11-19-2025').toISOString() };
-      const res = await request(app)
-        .post('/api/reservations')
-        .send(mockData);
+    assert.equal(res.statusCode, 400);
+    assert.deepEqual(res.body, { error: 'Missing required fields: userId, movieId, showtimeId, seats' });
+  });
 
-      assert.equal(res.statusCode, 400);
-      assert.deepEqual(res.body, { error: 'Missing seats' });
-    });
+  it('should return 400 if missing showtimeId', async () => {
+    const reservationRequest = {
+      userId: 'userId',
+      movieId: 'movieId',
+      seats: ['A16'],
+    };
+    const res = await request(app)
+      .post('/api/reservations')
+      .send(reservationRequest);
 
-    it('should return 400 if date is missing', async (t) => {
-      t.mock.method(reservationService, 'createReservation', async () => { });
-      const mockData = { title: 'Title', seats: 2 };
-      const res = await request(app)
-        .post('/api/reservations')
-        .send(mockData);
+    assert.equal(res.statusCode, 400);
+    assert.deepEqual(res.body, { error: 'Missing required fields: userId, movieId, showtimeId, seats' });
+  });
 
-      assert.equal(res.statusCode, 400);
-      assert.deepEqual(res.body, { error: 'Missing date' });
-    });
+  it('should return 400 if missing seats', async () => {
+    const reservationRequest = {
+      userId: 'userId',
+      movieId: 'movieId',
+      showtimeId: 'showtimeId',
+    };
+    const res = await request(app)
+      .post('/api/reservations')
+      .send(reservationRequest);
+
+    assert.equal(res.statusCode, 400);
+    assert.deepEqual(res.body, { error: 'Missing required fields: userId, movieId, showtimeId, seats' });
   });
 });
